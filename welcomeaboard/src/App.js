973c092e23cb4b2f6b1a4f055a3f560156287583
Krzysztof Wawrzkiewicz <krzysztof.wawrzkiewicz@fenergo.com>
2024-09-12 16:00:53 +0200

    simple materialUI styling

--- welcomeaboard/src/App.js
+++ welcomeaboard/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Amplify } from 'aws-amplify';
 import { Authenticator, Button } from '@aws-amplify/ui-react';
-import { Container, Typography, Box } from '@mui/material';
+import { Container, Typography, Box, Card, CardContent, TextField } from '@mui/material';
 import awsconfig from './aws-exports';
 
 Amplify.configure(awsconfig);
@@ -25,22 +25,64 @@ function App() {
 
   return (
     <Container maxWidth="sm">
-      <Box sx={{ mt: 4, textAlign: 'center' }}>
-          <Authenticator>
-            {({ signOut, user }) => (
-              <div>
-                <h1>Welcome to WelcomeAboard</h1>
-                {user ? (
-                  <div>
-                    <h2>Hello, {user.username}</h2>
-                    <Button onClick={signOut}>Sign Out</Button>
-                  </div>
-                ) : (
-                  <div>Please sign in</div>
-                )}
-              </div>
-            )}
-        </Authenticator>
+      <Box sx={{ mt: 8, display: 'flex', justifyContent: 'center' }}>
+        <Card sx={{ width: '100%', maxWidth: 400, boxShadow: 3 }}>
+          <CardContent>
+            <Typography variant="h4" align="center" gutterBottom>
+              Welcome to WelcomeAboard
+            </Typography>
+            <Authenticator>
+              {({ signOut, user }) => (
+                <Box>
+                  {user ? (
+                    <Box textAlign="center">
+                      <Typography variant="h6" gutterBottom>
+                        Hello, {user.username}
+                      </Typography>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={signOut}
+                        sx={{ mt: 2 }}
+                      >
+                        Sign Out
+                      </Button>
+                    </Box>
+                  ) : (
+                    <Box>
+                      <Typography variant="body1" gutterBottom>
+                        Please sign in to continue.
+                      </Typography>
+                      <Box sx={{ mt: 2 }}>
+                        <TextField
+                          label="Email"
+                          fullWidth
+                          variant="outlined"
+                          margin="normal"
+                        />
+                        <TextField
+                          label="Password"
+                          type="password"
+                          fullWidth
+                          variant="outlined"
+                          margin="normal"
+                        />
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          fullWidth
+                          sx={{ mt: 2 }}
+                        >
+                          Sign In
+                        </Button>
+                      </Box>
+                    </Box>
+                  )}
+                </Box>
+              )}
+            </Authenticator>
+          </CardContent>
+        </Card>
       </Box>
     </Container>
   );
